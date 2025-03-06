14a80cbeef9e29a517e6078187d83c4ac909a784
Sahar Fayez <saharfayez2002@gmail.com>
2025-03-06 03:40:57 +0200

    Initial commit

---
+++ src/main/java/com/example/MovieManagementApp/MovieManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.MovieManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MovieManagementApp/MovieManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MovieManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
